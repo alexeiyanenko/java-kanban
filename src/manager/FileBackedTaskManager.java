eda84f967c834ad629a7c36d687aa438c2dfd658
alexeiyanenko <tech@yanenko.me>
2024-08-02 14:15:48 +0300

    Выполнить задание спринта 7

---
+++ src/manager/FileBackedTaskManager.java
@@ -0,0 +1,145 @@
+package manager;
+
+import tasks.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileBackedTaskManager extends InMemoryTaskManager {
+    private final File file;
+
+    public FileBackedTaskManager(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public void addTask(Task task) {
+        super.addTask(task);
+        save();
+    }
+
+    @Override
+    public void addEpic(Epic epic) {
+        super.addEpic(epic);
+        save();
+    }
+
+    @Override
+    public void addSubTask(SubTask subtask) {
+        super.addSubTask(subtask);
+        save();
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        super.updateTask(task);
+        save();
+    }
+
+    @Override
+    public void updateEpic(Epic epic) {
+        super.updateEpic(epic);
+        save();
+    }
+
+    @Override
+    public void updateSubTask(SubTask subtask) {
+        super.updateSubTask(subtask);
+        save();
+    }
+
+    @Override
+    public void removeTask(int id) {
+        super.removeTask(id);
+        save();
+    }
+
+    @Override
+    public void removeEpic(int id) {
+        super.removeEpic(id);
+        save();
+    }
+
+    @Override
+    public void removeSubTask(int id) {
+        super.removeSubTask(id);
+        save();
+    }
+
+    void save() {
+        try (BufferedWriter writer = Files.newBufferedWriter(file.toPath())) {
+            writer.write("id,type,name,status,description,epic\n");
+            for (Task task : getAllTasks()) {
+                writer.write(toString(task) + "\n");
+            }
+            for (Epic epic : getEpics()) {
+                writer.write(toString(epic) + "\n");
+                for (SubTask subtask : getSubTasksOfEpic(epic)) {
+                    writer.write(toString(subtask) + "\n");
+                }
+            }
+        } catch (IOException e) {
+            throw new ManagerSaveException("Error saving tasks", e);
+        }
+    }
+
+    private String toString(Task task) {
+        if (task instanceof SubTask) {
+            SubTask subtask = (SubTask) task;
+            return String.format("%d,SUBTASK,%s,%s,%s,%d",
+                    subtask.getId(), subtask.getName(), subtask.getStatus(),
+                    subtask.getDescription(), subtask.getEpicId());
+        } else if (task instanceof Epic) {
+            Epic epic = (Epic) task;
+            return String.format("%d,EPIC,%s,%s,%s",
+                    epic.getId(), epic.getName(), epic.getStatus(), epic.getDescription());
+        } else {
+            return String.format("%d,TASK,%s,%s,%s",
+                    task.getId(), task.getName(), task.getStatus(), task.getDescription());
+        }
+    }
+
+    public static FileBackedTaskManager loadFromFile(File file) {
+        FileBackedTaskManager manager = new FileBackedTaskManager(file);
+        try {
+            List<String> lines = Files.readAllLines(file.toPath());
+            for (String line : lines.subList(1, lines.size())) { // Skip header
+                Task task = fromString(line);
+                if (task instanceof Epic) {
+                    manager.addEpic((Epic) task);
+                } else if (task instanceof SubTask) {
+                    manager.addSubTask((SubTask) task);
+                } else {
+                    manager.addTask(task);
+                }
+            }
+        } catch (IOException e) {
+            throw new ManagerSaveException("Error loading tasks", e);
+        }
+        return manager;
+    }
+
+    private static Task fromString(String value) {
+        String[] fields = value.split(",");
+        int id = Integer.parseInt(fields[0]);
+        String type = fields[1];
+        String name = fields[2];
+        TaskStatus status = TaskStatus.valueOf(fields[3]);
+        String description = fields[4];
+
+        switch (type) {
+            case "TASK":
+                return new Task(id, name, description, status);
+            case "EPIC":
+                return new Epic(id, name, description, status);
+            case "SUBTASK":
+                int epicId = Integer.parseInt(fields[5]);
+                return new SubTask(epicId, id, name, description, status);
+            default:
+                throw new IllegalArgumentException("Unknown task type: " + type);
+        }
+    }
+}
+
